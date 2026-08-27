22a9a032750e7aea0a64a87e5ed582e7c9289023
Durgesh Kumar <mr.durgeshkumar55@gmail.com>
2026-08-27 21:56:41 +0530

    main screens

---
+++ app/src/main/java/com/inflame/Nutrihostel/presentation/Home/HomeScreen.kt
@@ -0,0 +1,26 @@
+package com.inflame.Nutrihostel.presentation.Home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Text(text = "Welcome to NutriHostel")
+        }
+    }
+}
